cc72c1baa65e052d56863ab1d2eca3560a42e655
Patrizia <patty_28@hotmail.it>
2015-04-27 23:46:09 +0200

    commit

--- cg_26/src/main/java/it/polimi/ingsw/cg_26/App.java
+++ cg_26/src/main/java/it/polimi/ingsw/cg_26/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Patt!!!" );
+        System.out.println( "Hello World Patty!!!" );
     }
 }
