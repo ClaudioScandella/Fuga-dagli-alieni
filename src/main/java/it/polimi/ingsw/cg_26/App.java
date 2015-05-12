af1ca09f4f3a1e73e95e4e3d214930515df4b9c1
Claudio <Claudio@10.169.163.146>
2015-05-12 16:38:10 +0200

    a

--- src/main/java/it/polimi/ingsw/cg_26/App.java
+++ src/main/java/it/polimi/ingsw/cg_26/App.java
@@ -8,10 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        System.out.println( "Hellold Patty!! Funziona!" );
-=======
-        System.out.println( "Hello world!!!" );
->>>>>>> branch 'master' of https://claudio_scandella@bitbucket.org/patrizia_porati/fuga_dagli_alieni.git
+        System.out.println( "bu" );
+        
     }
 }
