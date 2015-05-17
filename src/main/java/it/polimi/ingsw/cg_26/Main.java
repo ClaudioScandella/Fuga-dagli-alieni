cbc26afb2a19d5927fca53eabe2612e8e44dfdf1
Claudio <Claudio@Claudio-PC>
2015-05-17 15:56:02 +0200

    prova di funzionamento

---
+++ src/main/java/it/polimi/ingsw/cg_26/Main.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.cg_26;
+
+import java.util.*;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ProvaLetturaFile.main(args);
+		
+		
+		final MappaGalilei mappa=new MappaGalilei();
+		
+		
+		/*
+		int a=45;
+		int b=a%23;
+		System.out.println(b);
+		//*/
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+
+}
