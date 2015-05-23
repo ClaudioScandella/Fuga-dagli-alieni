e81a424e03166074d23909031a41bda8fe9205f7
Claudio <Claudio@Claudio-PC>
2015-05-23 16:51:11 +0200

    prova commit di una classe

---
+++ src/main/java/it/polimi/ingsw/cg_26/model/mazzi/MazzoScartiCarteSettore.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.cg_26.model.mazzi;
+
+
+import it.polimi.ingsw.cg_26.model.carte.CartaSettore;
+
+import java.util.ArrayList;
+
+public class MazzoScartiCarteSettore {
+	private ArrayList<CartaSettore> mazzoScartiCarteSettore;
+}
