d84b80ed0e6272c36938f80d116df74d92e86138
Patrizia <patty_28@hotmail.it>
2015-06-24 23:46:10 +0200

    modifica test oggetto

--- src/test/java/it/polimi/ingsw/cg_26/model/carte/CartaOggettoTest.java
+++ src/test/java/it/polimi/ingsw/cg_26/model/carte/CartaOggettoTest.java
@@ -15,6 +15,7 @@ public class CartaOggettoTest {
 		c1 = new CartaOggetto(TipoOggetto.DIFESA);
 	}
 
+	
 	@Test
 	public void testGetTipoOggetto() {
 		assertEquals(c1.getTipoOggetto(), TipoOggetto.DIFESA);
